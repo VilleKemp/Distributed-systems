92a2b4a8542038945749b0dda6a4554dcb598b06
Otso Suvilehto <otso.suvilehto@student.oulu.fi>
2017-04-19 15:00:32 +0300

    Nodes greet coordinator, workload selector added

--- async_server.py
+++ async_server.py
@@ -15,8 +15,55 @@ http=httplib2.Http()
 enable_pretty_logging()
 
 GATEWAY = "http://localhost:8888/startup/"
+COORDPORT = 'coordinatorPort'
+nodes = 'nodes'
+cache = {COORDPORT:"None", nodes:{}}
+
+server = None #server object
+
+class Node:
+    #"""Implements Node object logic"""
+
+    def __init__(self):
+        pass
+    def informCoordinator(self):
+        """send address to coordinator"""
+        COORDINATOR="http://localhost:%s/listnode/" % cache[COORDPORT]
+        response, content = http.request(COORDINATOR, method='POST', headers=None, body=str(sys.argv[1]))
+        print "Class Node: Port " + str(sys.argv[1]) + " sent to Coordinator in port: " + cache[COORDPORT]
+    def Bully(self,nodes):
+        """implements the Bully algorithm"""
+        pass
+
+
+
+class Coordinator:
+    #"""Implements Coordinator object logic"""
+    def __init__(self):
+        self.workerLoads = dict()
+
+    def selectWorker(self):
+        """selects the node with lowest workload for performing calculation"""
+        min_ = float("inf")
+        selected = None
+        if cache[nodes]:
+            for key, value in cache[nodes].items():
+                if str(value)< min_:
+                    selected = key
+            cache[nodes][selected]+=1 #+1 to selected workload
+
+        else:
+            print "class Coordinator selectWorker(): no cache[nodes], returning None"
+        return selected
+
+    def forwardCalculation(node):
+        """Fordwards calculations to a node"""
+        pass
+    def updateWorkerLoads(self):
+        pass
 
 class NodeHandler(tornado.web.RequestHandler):
+
     @tornado.web.asynchronous
     @gen.engine
     def get(self):
@@ -38,6 +85,7 @@ class NodeHandler(tornado.web.RequestHandler):
         #tornado.ioloop.IOLoop.instance().stop()
 
 class CoordinatorHandler(tornado.web.RequestHandler):
+
     @gen.coroutine
     def get(self):
         http_client = tornado.httpclient.AsyncHTTPClient()
@@ -45,18 +93,21 @@ class CoordinatorHandler(tornado.web.RequestHandler):
         response = yield http_client.fetch("http://localhost:8889/node/", method='GET')
 
         data=self.request.body
-        print "Coordinator GET: " + data
+        print "CoordinatorHandler GET: " + data
         self.write(response.body)
         print response
         self.finish()
 
     @gen.coroutine
     def post (self):
-        print "POST"
+        """Takes in data and requests calculations from node"""
+        print "CoordinatorHandler POST"
         print self.request.body
 
         http_client = tornado.httpclient.AsyncHTTPClient()
-        response = yield http_client.fetch("http://localhost:8890/node/", method='GET')
+        SLAVE = "http://localhost:%s/node/" % server.selectWorker()
+        print "CoordinatorHandler POST: Workload sent to " + SLAVE
+        response = yield http_client.fetch(SLAVE, method='GET')
         data=response.body
 
         self.write(data)
@@ -66,21 +117,57 @@ class CoordinatorHandler(tornado.web.RequestHandler):
         self.add_header('Access-Control-Allow-Origin', self.request.headers.get('Origin', '*'))
         self.add_header('Access-Control-Request-Method', 'POST')
 
+class ListNodeHandler(tornado.web.RequestHandler):
+    #A request handler for the coordinator
+    def initialize(self, cache):
+        self.cache=cache
 
-if __name__=="__main__":
+    @gen.coroutine
+    def post(self):
+        #post node's address to the coordinator
+        content = self.request.body
+        global cache
+        if int(content):
+            cache[nodes][str(content)]=0 #add node to worker list with 0 load
+            print "ListNodeHandler POST: Added node to cache: "+ str(content)
+        else:
+            print "ListNodeHandler POST: Couldn't convert content to int"
 
-    application = tornado.web.Application([
-        (r"/node/", NodeHandler),
-        (r"/coordinator/", CoordinatorHandler),
-        ], debug=1)
+
+def startServer():
+    """Greet Gateway and determine the role of the instance"""
     response, content = http.request(GATEWAY, method='GET', headers=None, body=None) #Send it off!
     print "Response from gateway on get: "+content
     if content == "None":
+        #Tell port number to Gateway and instantiate a coordinator
         print "sending port via post"
         post_data = {"port":sys.argv[1]}
         body = urllib.urlencode(post_data)
         response, content = http.request(GATEWAY, method='POST', headers=None, body=str(sys.argv[1]))
+        global server
+        server = Coordinator()
+        cache[COORDPORT]=sys.argv[1]
+    elif int(content):
+        cache[COORDPORT]=content
+        global server
+        server = Node()
+        server.informCoordinator()
 
+    return server
+
+def main():
+    #boot up and determine the role of the instance
+    startServer()
+
+
+if __name__=="__main__":
+
+    application = tornado.web.Application([
+        (r"/node/", NodeHandler, dict(cache=cache)),
+        (r"/coordinator/", CoordinatorHandler),
+        (r"/listnode/", ListNodeHandler, dict(cache=cache))
+        ], debug=1)
+    main()
     application.listen(int(sys.argv[1]))
     print "Server in port " + sys.argv[1]
     tornado.ioloop.IOLoop.instance().start()
--- gateway.py
+++ gateway.py
@@ -13,6 +13,11 @@ enable_pretty_logging()
 PORT = '8888'
 COORDPORT = 'coordinatorPort'
 data = {COORDPORT:"None"}
+class GameHandler(tornado.web.RequestHandler):
+    @gen.coroutine
+    def get(self):
+        self.render("game.html")
+
 class StartupHandler(tornado.web.RequestHandler):
 
     #Handles new servers connecting to the grid
@@ -20,24 +25,25 @@ class StartupHandler(tornado.web.RequestHandler):
         self.cache = cache
 
     def get(self):
-        #Tells connecting node the address of the coordinator
+        """Tells connecting node the address of the coordinator"""
         global data
         self.write(data[COORDPORT])
         print "In GET: data updated: "
         print data
 
     def post(self):
-        #receive coordinator's port number
-        port = self.request.body
+        """receive coordinator's port number, to be used only when the coordinator is down"""
+        content = self.request.body
         global data
         if data[COORDPORT]=="None":
-            data[COORDPORT]=port
-            print "new coordinatorPort: " + str(port)
+            data[COORDPORT]=content
+            print "new coordinatorPort: " + str(content)
         print "check" + str(data[COORDPORT])
 
 if __name__=="__main__":
 
     application = tornado.web.Application([
+        (r"/game/", GameHandler),
         (r"/startup/", StartupHandler, dict(cache = data)),
         ], debug=1)
     application.listen(int(PORT))
